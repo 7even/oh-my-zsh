f170ac188c37ef0634db8a9c6e3d7f4df3e20e1c
Vsevolod Romashov <7@7vn.ru>
2013-06-18 20:26:51 +0400

    small fixes in default recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,12 +1,12 @@
-unless node['oh-my-zsh']['users'].empty?
+unless node[:oh_my_zsh][:users].empty?
   include_recipe 'git'
   include_recipe 'zsh'
 end
 
-node['oh-my-zsh']['users'].each do |hash|
-  login = hash['login']
-  home  = hash['home'] || (login == "root" ? "/root" : "/home/#{login}")
-
+node[:oh_my_zsh][:users].each do |hash|
+  login = hash[:login]
+  home  = hash[:home] || `cat /etc/passwd | grep "#{hash[:login]}" | cut -d ":" -f6`.chop
+  
   git "#{home}/.oh-my-zsh" do
     repository 'git://github.com/robbyrussell/oh-my-zsh.git'
     user       login
@@ -14,25 +14,25 @@ node['oh-my-zsh']['users'].each do |hash|
     action     :sync
     depth      1
   end
-
+  
   template "#{home}/.zshrc" do
     source 'zshrc.erb'
     owner login
     mode '644'
-    variables({
-      :user           => login,
-      :theme          => hash['theme']          || node['oh-my-zsh']['theme'],
-      :case_sensitive => hash['case_sensitive'] || node['oh-my-zsh']['case_sensitive'],
-      :plugins        => hash['plugins']        || node['oh-my-zsh']['plugins'],
-      :autocorrect    => node['oh-my-zsh']['autocorrect']
-    })
+    variables(
+      user:           login,
+      theme:          hash[:theme]          || node[:oh_my_zsh][:theme],
+      case_sensitive: hash[:case_sensitive] || node[:oh_my_zsh][:case_sensitive],
+      plugins:        hash[:plugins]        || node[:oh_my_zsh][:plugins],
+      autocorrect:    node[:oh_my_zsh][:autocorrect]
+    )
   end
-
+  
   user login do
     action :modify
     shell '/bin/zsh'
   end
-
+  
   execute 'source /etc/profile to all zshrc' do
     command "echo 'source /etc/profile' >> /etc/zsh/zprofile"
     not_if  "[[ ! -f /etc/zsh/zprofile ]] || grep 'source /etc/profile' /etc/zsh/zprofile"
