7784989882e75ac9e26228d817b7b04f73f43ef8
Vsevolod Romashov <7@7vn.ru>
2014-03-01 03:16:43 +0300

    fix default recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,7 +5,7 @@ end
 
 node[:oh_my_zsh][:users].each do |hash|
   login = hash[:login]
-  home  = hash[:home] || (username == 'root' ? '/root' : "/home/#{username}")
+  home  = hash[:home] || (login == 'root' ? '/root' : "/home/#{username}")
   
   git "#{home}/.oh-my-zsh" do
     repository 'git://github.com/robbyrussell/oh-my-zsh.git'
